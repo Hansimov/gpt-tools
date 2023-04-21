214b1966a00cf861755c78d7485e85eb317065c3
Hansimov <591172499@qq.com>
2023-04-22 01:35:53 +0800

    [Feature] Implement a minimal working example, fix proxy issue, fix data path issue, and generalize configs

--- test_llama_index.py
+++ test_llama_index.py
@@ -1,20 +1,30 @@
 #%%
+"""
+* llama_index/examples/paul_graham_essay
+  * https://github.com/jerryjliu/llama_index/tree/main/examples/paul_graham_essay
+"""
 import os
-
-os.environ["OPENAI_API_KEY"] = "<OPENAI_API_KEY>"
+import json
+from pathlib import Path
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
-data_filename = "./what_i_worked_on.txt"
-index_filename = "./index_what_i_worked_on.json"
+with open(Path(__file__).parent / "secrets.json", "r") as rf:
+    secrets = json.load(rf)
+os.environ["OPENAI_API_KEY"] = secrets["openai_api_key"]
+
+for proxy_env in ["http_proxy", "https_proxy"]:
+    os.environ[proxy_env] = secrets["http_proxy"]
+
+# data_filename = "./what_i_worked_on.txt"
+index_filename = Path(__file__).parent / "index" / "what_i_worked_on_index.json"
 documents = SimpleDirectoryReader("data").load_data()
 
-# Modify paths of `vocab_bpe` and `encoder_json` in following location:
-#   [C:\Python39\lib\site-packages\tiktoken_ext\openai_public.py]: Line 12-13
-if not os.path.exists(index_filename):
+overwrite_index = False
+if not os.path.exists(index_filename) or overwrite_index:
     index = GPTSimpleVectorIndex.from_documents(documents)
     index.save_to_disk(index_filename)
-else:
-    index = GPTSimpleVectorIndex.load_from_disk(index_filename)
 
-response = index.query("What did the author do growing up?")
+new_index = GPTSimpleVectorIndex.load_from_disk(index_filename)
+
+response = new_index.query("What did the author do growing up? 请用中文回答。")
 print(response)
